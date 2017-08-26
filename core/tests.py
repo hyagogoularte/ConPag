aa1eb9779c0934bdff666767b61e00615e3a6436
Hyago Goularte <hyagogoularte@gmail.com>
2017-08-26 18:45:06 -0300

    Feito alguns testes

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test import Client
 
 # Create your tests here.
+class TestarChamadas(TestCase):
+
+    def teste_chamada_home_view_sem_logar(self):
+        response = self.client.get('/home/', follow=True)
+        self.assertRedirects(response, '/?next=/home/')
+
+
+class TestarTemplates(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def teste_chamada_home_view_logando(self):
+        self.client.login(username='gerson', password='qwer1234')
+        response = self.client.get(reverse('home'), follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base.html')
--- lancamento/tests.py
+++ lancamento/tests.py
@@ -1,12 +1,28 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test import Client
 
 # Create your tests here.
 class TestarChamadas(TestCase):
+    def setUp(self):
+        self.client = Client()
 
-    def teste_chamada_home_view_sem_logar(self):
-        response = self.client.get('/home/', follow=True)
-        self.assertRedirects(response, '/login/')
+    def teste_chamada_lancamentos_view_sem_logar(self):
+        response = self.client.get('/lancamentos/', follow=True)
+        self.assertRedirects(response, '/?next=/lancamentos/')
 
-        response = self.client.post('/home/', follow=True)
-        self.assertRedirects(response, '/login/')
+    def teste_chamada_novo_lancamentos_view_sem_logar(self):
+        response = self.client.get(reverse('novo-lancamento'), follow=True)
+        self.assertRedirects(response, '/?next=/lancamentos/novo/')
 
+
+class TestarTemplates(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def teste_chamada_lancamento_view_logando(self):
+        self.client.login(username='gerson', password='qwer1234')
+        response = self.client.get(reverse('home'), follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base.html')
